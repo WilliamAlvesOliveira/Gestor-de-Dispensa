c10e1952a9f21b97bf6895c26649ad65645cf9bb
WilliamAlvesOliveira <27will.ramone@gmail.com>
2025-02-26 17:35:27 -0300

    function is conected bug fixed
    correção na função connection_test

--- src/db.py
+++ src/db.py
@@ -3,6 +3,7 @@ from mysql.connector import Error ## Importa a classe Error para tratar exceçõ
 
 def connection_test():
     # Tenta estabelecer uma conexão com o banco de dados MySQL usando os parâmetros fornecidos
+    db = None #inicia a variavel db
     try:
         db = mysql.connector.connect(
             host = "localhost",
@@ -12,13 +13,14 @@ def connection_test():
         )
 
         if db.is_connected(): #verifica se a conexão foi bem-sucedida
-            return "A conexão com o banco de dados foi bem-sucedida."
-        else:
-            return "A conexão falhou por um motivo desconhecido."
+            return "✅ Conexão bem-sucedida com o banco de dados 'dispensa'."
         
-    except Error as err: #capitura qualquer exceção gerada durante o processo de conexão
-        return f"Erro ao conectar ao banco de dados: {err}" #retorna a mensagem com o erro
+        return "⚠️ A conexão falhou por um motivo desconhecido."
+        
+    except Error as err: #captura qualquer exceção gerada durante o processo de conexão
+        return f"❌ Erro ao conectar ao banco de dados: {err}" #retorna a mensagem com o erro
     
     finally:
         if db and db.is_connected():
-            db.close()  # Fecha a conexão com o banco de dados
+            db.close()  # Fecha a conexão apenas se estiver ativa
+
